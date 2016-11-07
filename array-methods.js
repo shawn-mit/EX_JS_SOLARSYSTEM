6b2d81e93c09ca9fb0c8e3ee2510294d3f43c259
Shawn M <forgeforza@outlook.com>
2016-11-07 00:40:47 -0600

    intial commit

---
+++ array-methods.js
@@ -0,0 +1,50 @@
+var planets = ["mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune"];
+
+/*
+ Use the forEach method to add the name of each planet
+ to a div element in your HTML with an id of "planets".
+*/
+
+
+planets.forEach(function(el) {
+	console.log(el);
+	document.getElementById("planets").innerHTML;
+	
+		el.innerHTML  
+		//console.log (el);
+});
+
+
+
+//document.getElementById("planets").innerHTML = el
+
+
+
+/*
+
+var a = ["a", "b", "c"];
+
+a.forEach(function(element) {
+    console.log(element);
+});
+
+// a
+// b
+// c
+
+
+
+*/
+
+	//document.getElementById("demo").innerHTML = "Paragraph changed!";
+
+
+// Use the map method to create a new array where the first letter of each planet is capitalized
+
+// Use the filter method to create a new array that contains planets with the letter 'e'
+
+// Use the reduce method to create a sentence from the words in the following array
+
+var words = ["The", "early", "bird", "might", "get", "the", "worm", "but", "the", "second", "mouse", "gets", "the", "cheese"];
+
+
